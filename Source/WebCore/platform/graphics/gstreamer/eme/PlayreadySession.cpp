6a55aea9b31560ef945787709372485c2cac3251
Enrique Ocaña González <eocanha@igalia.com>
2017-05-19 18:26:16 +0200

    [GStreamer][EME][PlayReady] Deinitialize DRM_APP_CONTEXT on PlayreadySession destruction

--- Source/WebCore/platform/graphics/gstreamer/eme/PlayreadySession.cpp
+++ Source/WebCore/platform/graphics/gstreamer/eme/PlayreadySession.cpp
@@ -112,6 +112,8 @@ PlayreadySession::~PlayreadySession()
 {
     GST_DEBUG("Releasing resources");
 
+    Drm_Uninitialize (m_poAppContext);
+
     if (DRM_REVOCATION_IsRevocationSupported())
         SAFE_OEM_FREE(m_pbRevocationBuffer);
 
